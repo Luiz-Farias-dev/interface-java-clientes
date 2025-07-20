666287a91f3c5edf38e51c4c04a1909d9c4e38a0
Luiz-Farias-dev <luizferreirafariasn@gmail.com>
2025-07-20 20:13:04 -0300

    Projeto pessoal para estudo, primeiro commit sistema quase completo

---
+++ src/br/com/luizfarias/clientes/Main.java
@@ -0,0 +1,11 @@
+package br.com.luizfarias.clientes;
+
+import br.com.luizfarias.clientes.apresentacao.TelaCadastro;
+import br.com.luizfarias.clientes.dominio.Cliente;
+
+public class Main {
+    public static void main(String[] args) {
+        TelaCadastro telaCadastro = new TelaCadastro();
+        telaCadastro.setVisible(true);
+    }
+}
---
+++ src/br/com/luizfarias/clientes/apresentacao/TelaCadastro.java
@@ -0,0 +1,90 @@
+package br.com.luizfarias.clientes.apresentacao;
+
+import br.com.luizfarias.clientes.dominio.Cliente;
+import br.com.luizfarias.clientes.dominio.enums.TipoSexo;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TelaCadastro extends JFrame {
+
+    private JLabel labelNome;
+    private JLabel labelCpf;
+    private JLabel labelSexo;
+
+    private JTextField campoNome;
+    private JTextField campoCpf;
+    private JComboBox<TipoSexo> campoSexo;
+
+    private JButton botaoSalvar;
+
+    public TelaCadastro() {
+        construirTela();
+    }
+
+    private void construirTela() {
+        setSize(600, 500);
+        setTitle("Cadastro de Clientes");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        this.adicionarCampos();
+        this.adicionarBotoes();
+        this.adicionarComponentesFoto();
+    }
+
+    private void adicionarCampos() {
+        this.labelNome = new JLabel("Nome:");
+        labelNome.setBounds(20, 20, 200, 20);
+        getContentPane().add(labelNome);
+
+        campoNome = new JTextField();
+        campoNome.setBounds(20, 40, 200, 20);
+        getContentPane().add(campoNome);
+
+        this.labelCpf = new JLabel("CPF:");
+        labelCpf.setBounds(20, 60, 200, 20);
+        getContentPane().add(labelCpf);
+
+        campoCpf = new JTextField();
+        campoCpf.setBounds(20, 80, 200, 20);
+        getContentPane().add(campoCpf);
+
+        this.labelSexo = new JLabel("Sexo:");
+        labelSexo.setBounds(20, 100, 200, 20);
+        getContentPane().add(labelSexo);
+
+        TipoSexo[] tipoSexo = {null, TipoSexo.M, TipoSexo.F, TipoSexo.O};
+        campoSexo = new JComboBox<>(tipoSexo);
+        campoSexo.setBounds(20, 120, 200, 20);
+        getContentPane().add(campoSexo);
+    }
+
+    private void adicionarBotoes() {
+        botaoSalvar = new JButton("Salvar");
+        botaoSalvar.setBounds(20, 160, 100, 20);
+
+        ActionListener acaoBotaoSalvar = this.botaoSalvarActionListener();
+        botaoSalvar.addActionListener(acaoBotaoSalvar);
+
+        getContentPane().add(botaoSalvar);
+    }
+
+    private void adicionarComponentesFoto() {}
+
+    private ActionListener botaoSalvarActionListener(){
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                Cliente cliente = new Cliente();
+                cliente.setNome(campoNome.getText());
+                cliente.setCpf(campoCpf.getText());
+                cliente.setSexo((TipoSexo) campoSexo.getSelectedItem());
+
+                JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
+            }
+        };
+    }
+}
+
